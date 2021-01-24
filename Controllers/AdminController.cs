983fb6dcf3d3f74f5bae4344f016c6e2b29e35bc
Randy Parker <randallbparker@gmail.com>
2021-01-24 08:08:47 -0600

    admin controller edit user role additions

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DarnTheLuck.Controllers
@@ -9,10 +10,12 @@ namespace DarnTheLuck.Controllers
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public AdminController(RoleManager<IdentityRole> roleManager)
+        public AdminController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
-            this._roleManager = roleManager;
+            _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -58,6 +61,92 @@ namespace DarnTheLuck.Controllers
 
             return View(model);
         }
+    
+
+
+        [HttpGet]
+        public async Task<IActionResult> EditUsersInRole(string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                RedirectToAction("Index"); // ToDo: return error page
+            }
+
+            ViewBag.roleId = roleId;
+
+            var model = new List<UserRoleViewModel>();
+
+            /*****
+             * LEARNING OPPORTUNITY:
+             * 
+             * _userManager.Users seems to keep the database connection OPEN
+             * 
+             * using .ToList() seems to save the Users to a List and close the connection
+             * 
+             *****/
+
+            foreach (var user in _userManager.Users.ToList()) 
+            {
+                UserRoleViewModel userRoleViewModel = new UserRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName
+                };
+
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    userRoleViewModel.IsSelected = true;
+                }
+                else
+                {
+                    userRoleViewModel.IsSelected = false;
+                }
+
+                model.Add(userRoleViewModel);
+            }
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> model, string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                return View("Index"); // ToDo: Error page
+            }
+
+            for (int i = 0; i < model.Count; i++)
+            {
+                var user = await _userManager.FindByIdAsync(model[i].UserId);
+
+                IdentityResult result;
+
+                if (model[i].IsSelected && !(await _userManager.IsInRoleAsync(user, role.Name)))
+                {
+                    result = await _userManager.AddToRoleAsync(user, role.Name);
+                }
+                else if (!model[i].IsSelected && await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    result = await _userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                else
+                {
+                    continue;
+                }
+
+                if (result.Succeeded && i > (model.Count))
+                    return RedirectToAction("Index"); // EditRole - we did not do this
+            }
+
+            return RedirectToAction("Index"); // EditRole - we did not do this .. the example redirects an edit page
+        }
+
     }
 }
 
@@ -72,4 +161,6 @@ namespace DarnTheLuck.Controllers
  * 
  * TODO: Tech and Admin user roles
  * Advanced features like tech groups and authorization links (allow others to view Ticket) can likely be simple database functions.
+ * 
+ * TODO: EVERY View will need to test for NULL - If a user types an URL that produces a NULL we need to catch and deal with this
  */
