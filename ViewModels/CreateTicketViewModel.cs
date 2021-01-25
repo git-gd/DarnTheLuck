4b2c2c6aa7c26010ca9e3bfa2aea11f5f861eea1
Randy Parker <randallbparker@gmail.com>
2021-01-24 19:56:45 -0600

    data annotation prompts

--- ViewModels/CreateTicketViewModel.cs
+++ ViewModels/CreateTicketViewModel.cs
@@ -5,11 +5,14 @@ namespace DarnTheLuck.ViewModels
     public class CreateTicketViewModel
     {
         [Required(ErrorMessage="Contact Name is required")]
+        [Display(Prompt = "Enter a contact name")]
         public string ContactName { get; set; }
         [Required(ErrorMessage="Contact Phone is required")]
+        [Display(Prompt = "Enter a contact phone number")]
         [Phone]
         public string ContactPhone { get; set; }
         [Required(ErrorMessage="Contact Email Address is required")]
+        [Display(Prompt="Enter a contact email address")]
         [EmailAddress]
         public string ContactEmail { get; set; }
         public string TicketNotes { get; set; }
