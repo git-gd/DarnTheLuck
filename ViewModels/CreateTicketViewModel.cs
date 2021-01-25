0794c6dc0e7e4c0d6d22289e29b09e9d4138c6c0
Randy Parker <randallbparker@gmail.com>
2021-01-24 19:48:38 -0600

    updated annotations

--- ViewModels/CreateTicketViewModel.cs
+++ ViewModels/CreateTicketViewModel.cs
@@ -4,15 +4,16 @@ namespace DarnTheLuck.ViewModels
 {
     public class CreateTicketViewModel
     {
-        [Required]
+        [Required(ErrorMessage="Contact Name is required")]
         public string ContactName { get; set; }
-        [Required]
+        [Required(ErrorMessage="Contact Phone is required")]
+        [Phone]
         public string ContactPhone { get; set; }
-        [Required]
+        [Required(ErrorMessage="Contact Email Address is required")]
+        [EmailAddress]
         public string ContactEmail { get; set; }
         public string TicketNotes { get; set; }
         public string Model { get; set; }
         public string Serial { get; set; }
     }
 }
-//TODO: finish setting up validation
