02629e2d0db350fe01df52f1400892799d835268
Randy Parker <randallbparker@gmail.com>
2021-02-18 07:47:27 -0600

    removed using

--- Controllers/AjaxController.cs
+++ Controllers/AjaxController.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json.Linq;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
