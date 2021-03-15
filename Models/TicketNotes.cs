8d425c982f1288e697df364257bcfb85e6baf751
Randy Parker <randallbparker@gmail.com>
2021-03-15 09:57:41 -0500

    TicketNotes class created to replace TicketNotes string in Ticket model

---
+++ Models/TicketNotes.cs
@@ -0,0 +1,11 @@
+﻿namespace DarnTheLuck.Models
+{
+    public class TicketNotes
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public Ticket Ticket { get; set; }
+
+        public string Note { get; set; }
+    }
+}
