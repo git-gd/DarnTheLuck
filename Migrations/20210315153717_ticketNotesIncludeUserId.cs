f37aedf57a145102430897a64f6030616012b61b
Randy Parker <randallbparker@gmail.com>
2021-03-15 10:38:49 -0500

    added UserId to TicketNotes to track users that create notes

---
+++ Migrations/20210315153717_ticketNotesIncludeUserId.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DarnTheLuck.Migrations
+{
+    public partial class ticketNotesIncludeUserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "TicketNotes",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketNotes_UserId",
+                table: "TicketNotes",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TicketNotes_AspNetUsers_UserId",
+                table: "TicketNotes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TicketNotes_AspNetUsers_UserId",
+                table: "TicketNotes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TicketNotes_UserId",
+                table: "TicketNotes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "TicketNotes");
+        }
+    }
+}
--- Models/TicketNotes.cs
+++ Models/TicketNotes.cs
@@ -1,11 +1,14 @@
-﻿namespace DarnTheLuck.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DarnTheLuck.Models
 {
     public class TicketNotes
     {
         public int Id { get; set; }
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
         public int TicketId { get; set; }
         public Ticket Ticket { get; set; }
-
         public string Note { get; set; }
     }
 }
